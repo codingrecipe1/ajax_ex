329c8b3b89774c712a64fb237ed5f659b4ea0b71
codingrecipe1 <codingrecipe1@gmail.com>
2022-12-22 13:38:57 +0900

    01_프로젝트 세팅

---
+++ README.md
@@ -0,0 +1,18 @@
+## 강의 주제
+- ajax 다뤄보기 
+
+## 사용 Dependency
+- Lombok
+- Spring Web
+- Thymeleaf
+
+## 주요 내용 
+- front 페이지에서 ajax요청을 어떻게 보내고, Controller에서 어떻게 받는지 알아봅니다.
+- ResponseBody, ResponseEntity 리턴을 활용해 봅니다. 
+- ajax에서 리턴을 받아서 javascript로 어떻게 활용하는지 알아봅니다. 
+
+## ajax 활용?
+- 이메일 또는 아이디 중복체크 
+- 좋아요 또는 싫어요 
+- 찜 
+- 댓글 작성 
---
+++ src/main/java/com/codingrecipe/ajaxEx/AjaxExApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.ajaxEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxExApplication.class, args);
+	}
+
+}
