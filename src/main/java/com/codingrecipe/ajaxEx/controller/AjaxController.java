60ff653b3ca1159cbd0c99257029c6aa4496dc3b
codingrecipe1 <codingrecipe1@gmail.com>
2022-12-22 14:23:58 +0900

    03_ex02 post 요청하기, ex03 get & paramter 요청하기

--- src/main/java/com/codingrecipe/ajaxEx/controller/AjaxController.java
+++ src/main/java/com/codingrecipe/ajaxEx/controller/AjaxController.java
@@ -2,12 +2,29 @@ package com.codingrecipe.ajaxEx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxController {
     @GetMapping("/ex01")
     public String ex01() {
         System.out.println("AjaxController.ex01");
-        return "index"; // .html
+        return "index"; // index.html에 작성된 내용이 출력됨
     }
+
+    @PostMapping("/ex02")
+    public @ResponseBody String ex02() {
+        System.out.println("AjaxController.ex02");
+        return "안녕하세요"; // 리턴값이 그대로 출력됨
+    }
+
+    @GetMapping("/ex03")
+    public @ResponseBody String ex03(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        return "ex03메서드 호출 완료";
+    }
+
 }
--- src/main/java/com/codingrecipe/ajaxEx/controller/AjaxViewController.java
+++ src/main/java/com/codingrecipe/ajaxEx/controller/AjaxViewController.java
@@ -9,4 +9,14 @@ public class AjaxViewController {
     public String ajaxEx01() {
         return "ajax-ex-01";
     }
+
+    @GetMapping("/ajax-ex-02")
+    public String ajaxEx02() {
+        return "ajax-ex-02";
+    }
+
+    @GetMapping("/ajax-ex-03")
+    public String ajaxEx03() {
+        return "ajax-ex-03";
+    }
 }
